ef848aa0a58635ffe6ca236394d3e103507eca4e
Maria Máyra <112517512+mayx2@users.noreply.github.com>
2024-12-12 00:09:36 -0300

    Delete 6.js

--- 6.js
+++
@@ -1,6 +0,0 @@
-let numero = +prompt("Digite um número:")
-let p = document.getElementById("tabuada")
-for (i = 1; i <= 10; i++) {
-    let mult = numero * i
-    p.textContent += `${numero}x${i}=${mult} | `;
-}
